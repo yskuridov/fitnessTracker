bc928c810f2064528ae9e45b1699b598a8547223
yskuridov <yskuridov@gmail.com>
2024-02-16 11:00:27 -0500

    Initial commit

---
+++ fitnessTracker/src/main/java/com/example/fitnesstracker/FitnessTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.fitnesstracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FitnessTrackerApplication.class, args);
+    }
+
+}
